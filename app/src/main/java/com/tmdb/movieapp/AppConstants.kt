19f89954dc79b93acd0d6174144da5f9b3614045
Gergely Szebenyi <v-gszebenyi@expediagroup.com>
2023-05-29 01:11:45 +0200

    api key removed from AppConstants

--- app/src/main/java/com/tmdb/movieapp/AppConstants.kt
+++ app/src/main/java/com/tmdb/movieapp/AppConstants.kt
@@ -1,8 +1,7 @@
 package com.tmdb.movieapp
 
 object AppConstants {
-    // TODO:  Hide API_KEY
-    const val API_KEY = "555dd34b51d2f5b7f9fdb39e04986933"
+    const val API_KEY = BuildConfig.API_KEY
     const val IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
     const val BASE_URL = "https://api.themoviedb.org/3/"
 }
